124277306784032be4fd1cf3c955168b4b021618
renzon <renzon@gmail.com>
2017-07-08 11:55:24 -0300

    Removed params from CPFFormField

--- localflavor/br/forms.py
+++ localflavor/br/forms.py
@@ -18,10 +18,11 @@ from .br_states import STATE_CHOICES
 
 
 class CPFFormField(EmptyValueCompatMixin, CharField):
-    def __init__(self, max_length=14, min_length=11, strip=True,
-                 empty_value='', *args, **kwargs):
+    def __init__(self, max_length=14, min_length=11, *args, **kwargs):
         super(CPFFormField, self).__init__(
-            max_length, min_length, strip, empty_value, *args, **kwargs)
+            max_length=max_length,
+            min_length=min_length,
+            *args, **kwargs)
 
     def validate(self, value):
         value = super(CPFFormField, self).validate(value)
